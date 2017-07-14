5e2bedf864418532762f539d016678af1ee4f41a
nikrb <nik_rb@yahoo.com>
2017-07-14 23:37:19 +0100

    display basic vector map of world

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import world_map from './WRLD-EPS-02-0015.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const wrapper = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "flex-start"
+    };
+    const style = {
+      width: "600px"
+    };
     return (
-      <div className="App">
+      <div style={wrapper}>
+        <img style={style} src={world_map} alt="world map" />
       </div>
     );
   }
