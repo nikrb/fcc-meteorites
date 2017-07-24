1dd400a428db0ab7fd78edd0e941d48e1b03f951
nikrb <nik_rb@yahoo.com>
2017-07-24 16:43:03 +0100

    calibrate map lat/lng

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Actions from './Actions';
 import Chart from './components/Chart';
 import './App.css';
 
+
 class App extends Component {
   state = {
-    features: [ {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          6.08333,
-          50.775
-        ]
-      }
-    }]
+    features: []
   };
+  calibration = [
+    {place: "england", lng:52.152037, lat:-0.588398},
+    {"place": "alaska", lng:65.716997, lat:-154.443759},
+    {"place": "russia",lng:66.359628, lat: 163.543406},
+    {"place": "antarctica",lng:-75.872892, lat:-87.896526},
+    {"place": " east antarctica",lng:-72.216494, lat:167.737049}
+  ];
   componentWillMount = () => {
     // Actions.getData()
     // .then( (response) => {
@@ -23,12 +23,13 @@ class App extends Component {
   };
   render() {
     const data = this.state.features.map( (d) => {
-      return { lat: d.geometry.coordinates[0], lng: d.geometry.coordinates[1] };
+      return { lat: d.geometry.coordinates[1], lng: d.geometry.coordinates[0] };
     });
+    // box top,left,bottom,right
     return (
       <div className="App">
         <h1>Meteorites</h1>
-        <Chart width={600} data={data} />
+        <Chart width={600} data={data} box={[40,30,420,575]} calibration={this.calibration} />
       </div>
     );
   };
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import * as d3 from 'd3';
 
 import world_map from './WRLD-EPS-02-0015.svg';
 
 export default class Chart extends React.Component {
   render = () => {
-    const {width} = this.props;
+    const {width, box} = this.props;
     const wrapper = {
       position: "relative",
       width
@@ -16,11 +17,20 @@ export default class Chart extends React.Component {
     const meteor_overlay = {
       position: "absolute",
       width,
+      height: width+"px"
     };
-    const meteorites = this.props.data.map( (d,i) => {
+    const xScale = d3.scaleLinear()
+      .domain( [-180, 180])
+      .range( [box[1], box[3]-box[1]]);
+    const yScale = d3.scaleLinear()
+      .domain( [90,-90])
+      .range( [box[0]+20, box[2]-box[0]]);
+    let data = this.props.data;
+    if( data.length === 0) data = this.props.calibration;
+    const meteorites = data.map( (d,i) => {
       const style={
-        cx:d.lat,
-        cy:d.lng,
+        cx: xScale( d.lat),
+        cy: yScale( d.lng),
         r:2,
         fill: 'black'
       };
@@ -33,6 +43,8 @@ export default class Chart extends React.Component {
         <img src={world_map} style={map_style} alt="" />
         <svg style={meteor_overlay}>
           {meteorites}
+          <rect x={box[1]} y={box[0]} width={box[3]-box[1]} height={box[2]-box[0]}
+            style={{stroke:"red", strokeWidth:"1", fill:"transparent"}}/>
         </svg>
       </div>
     );
